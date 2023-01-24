e01895d87db54e71ccb84324220a5b31abb3e303
Luke Fosbrook <luke.fosbrook@outlook.com>
2023-01-24 18:45:35 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XLN_Fault_Report_System.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
