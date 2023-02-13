10a15ded440558a41d1fe07045fd4b4f8382da1e
Luke Fosbrook <luke.fosbrook@outlook.com>
2023-02-13 15:53:20 +0000

    error form work

---
+++ Controllers/ErrorForm.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using XLN_Fault_Report_System.Models;
+
+
+namespace xlnWork.Controllers;
+
+public class ErrorForm : Controller
+{
+
+
+    public ErrorForm()
+    {
+
+    }
+
+    public IActionResult WarningPage()
+    {
+        return View();
+    }
+
+    public IActionResult Index()
+    {
+
+        return View();
+    }
+
+    //[HttpPost]
+    public IActionResult Diagnostics()
+    {
+        return View();
+    }
+
+}
+
