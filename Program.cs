b5860cdb3e8a3d94e05d364d2ba3b28d11b51d9b
Luke Fosbrook <luke.fosbrook@outlook.com>
2023-03-27 16:00:26 +0100

    more styling changes

--- Program.cs
+++ Program.cs
@@ -39,6 +39,6 @@ app.UseSession();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{controller=ErrorForm}/{action=CustomerDetails}/{id?}");
 
 app.Run();
