d32daf7386ecf453da1364c06db51dd5513280d6
Luke Fosbrook <luke.fosbrook@outlook.com>
2023-03-28 13:13:49 +0100

    time

--- Services/Services.cs
+++ Services/Services.cs
@@ -68,7 +68,7 @@ namespace XLN_Fault_Report_System.Services
 			foreach (Fault f in faults)
 			{
 				var fault = _context.Faults.Where(a => a.FaultId == f.FaultId).FirstOrDefault();	
-				DateTime faultSubmitDate = DateTime.Parse(f.Time, CultureInfo.InvariantCulture);
+				DateTime faultSubmitDate = DateTime.ParseExact(f.Time, "dd/MM/yyyy", CultureInfo.InvariantCulture);
 				TimeSpan diff = now - faultSubmitDate;	
 				if (diff.Minutes > 15 && fault.Status == "Fault report pending")
 				{
