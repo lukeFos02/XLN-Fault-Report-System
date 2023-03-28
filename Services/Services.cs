895411a50df35b3acc6b2e593548394d2cc7dd1a
Luke Fosbrook <luke.fosbrook@outlook.com>
2023-03-28 13:28:40 +0100

    time

--- Services/Services.cs
+++ Services/Services.cs
@@ -63,7 +63,7 @@ namespace XLN_Fault_Report_System.Services
 		}
 		public void UpdateFaultStatus(List<Fault> faults)
 		{
-			DateTime now = DateTime.Now.ToString("dd/MM/yyyy HH,mm,ss");
+			DateTime now = DateTime.Now.ToString("dd/MM/yyyy HH,mm,ss", CultureInfo.InvariantCulture);
 
 			foreach (Fault f in faults)
 			{
